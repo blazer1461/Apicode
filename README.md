352f872cf2a58bdcec818b1c4144b68d43a67824
miishin <miraisahara@miraimacbookair.campus.sjcny.edu>
2015-07-28 14:05:13 -0400

    FUCK THE CHINESE

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Apicode
+# youtubesearch
--- app.py
+++ app.py
@@ -1,50 +1,27 @@
-__author__ = 'blazer1461'
-import apicode
 from flask import Flask, render_template, request
-app= Flask (__name__)
+import steam
+import youtube
 
+app = Flask(__name__)
 
-def binary_search_appid(alist, id):
-    start = 0
-    end = len(alist) - 1
-    while (end >= start):
-        mid = (end - start) / 2 + start
-        if alist[mid]['appid'] == id:
-            # found it
-            return alist[mid]['name']
-        if alist[mid]['appid'] > id:
-            end = mid -1
-        else:
-            start = mid + 1
-    # if we are here (outside the while loop), then we didn't find it
-    return None
+@app.route("/")
+def main():
 
+    return render_template("basic.html")
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/search", methods = ["POST"])
+def search():
+    name = request.form["steamID"]
+    games = youtube.steam_return(name)
+    blah = []
+    for a in xrange(len(games)):
+        blah.append(a)
+    return render_template("search.html")
 
-def steam_return():
-    if request.method == "GET":
-        return render_template("base.html", username="Steam Search")
-    elif request.method == "POST":
-        user= request.form["username"]
-        dict= apicode.steamid_conversion(user)
-        game_dict= dict["games"]
-        w= {}
-        gname = {}
+@app.route("/videos/<gameName>", methods = ["POST"])
+def videos():
 
-        name_id= apicode.converting_ids_to_names()
-        for item in game_dict:
-            appid= item["appid"]
-            playtime= item["playtime_forever"]
-            w[appid]= playtime / 60
-            gname[appid] = binary_search_appid(name_id, appid)
-        gname_list= gname.values()
-
-
-        return render_template("games.html", username= user, games= w, names=gname, list= gname_list)
-
+    return render_template("videos.html")
+    
 if __name__ == "__main__":
-
-    app.debug= True
-    app.run(host= '0.0.0.0', port= 12345)
-
+    app.run()
